5a7322f8a4e684c727ad2a0dcdf66a77cf04109f
Con_Sen <90837200+vietnvph24355@users.noreply.github.com>
2024-07-18 10:33:59 +0700

    login thanh coong

--- src/main/java/com/example/bee/BeeApplication.java
+++ src/main/java/com/example/bee/BeeApplication.java
@@ -8,15 +8,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class BeeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BeeApplication.class, args);
-        // Tạo mật khẩu và mã hóa nó
-        String rawPassword = "123";
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String encodedPassword = encoder.encode(rawPassword);
-
-        // In ra mật khẩu đã mã hóa
-        System.out.println("Mật khẩu gốc: " + rawPassword);
-        System.out.println("Mật khẩu đã mã hóa: " + encodedPassword);
+//        SpringApplication.run(BeeApplication.class, args);
+//        // Tạo mật khẩu và mã hóa nó
+//        String rawPassword = "123";
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        String encodedPassword = encoder.encode(rawPassword);
+//
+//        // In ra mật khẩu đã mã hóa
+//        System.out.println("Mật khẩu gốc: " + rawPassword);
+//        System.out.println("Mật khẩu đã mã hóa: " + encodedPassword);
 
     }
 
