09d1a5f2db4c4a4a3a4388bfe52aefa51c46e480
VuMaiQuan <vumaiquan28062002@gmail.com>
2024-07-19 23:00:14 +0700

    len code

---
+++ src/main/java/com/example/bee/model/request/update_request/UpdatedGiaoDichRequest.java
@@ -0,0 +1,42 @@
+package com.example.bee.model.request.update_request;
+
+
+import com.example.bee.common.CommonEnum;
+import com.example.bee.entity.HoaDon;
+import com.example.bee.entity.PhuongThucThanhToan;
+import com.example.bee.entity.TaiKhoan;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UpdatedGiaoDichRequest {
+
+    private Long id;
+
+    private String maGiaoDich;
+
+    private BigDecimal soTienGiaoDich;
+
+    private LocalDateTime ngayThanhToan;
+
+    private LocalDateTime ngayTao;
+
+    private LocalDateTime ngaySua;
+
+    @Enumerated(EnumType.STRING)
+    private CommonEnum.TrangThaiGiaoDich trangThaiGiaoDich;
+
+    private HoaDon hoaDon;
+
+    private TaiKhoan taiKhoan;
+
+    private PhuongThucThanhToan phuongThucThanhToan;
+
+}
---
+++ src/main/java/com/example/bee/model/request/update_request/UpdatedPhuongThucThanhToanRequest.java
@@ -0,0 +1,16 @@
+package com.example.bee.model.request.update_request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdatedPhuongThucThanhToanRequest {
+
+    private String ten;
+
+    private String ma;
+
+    private Integer trang_thai;
+
+}
