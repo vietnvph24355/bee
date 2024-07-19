0c484c8f7ac9d6700e18d31f08e580210fc91f40
VuMaiQuan <vumaiquan28062002@gmail.com>
2024-07-19 16:59:20 +0700

    len

--- src/main/java/com/example/bee/service/MauSacService.java
+++ src/main/java/com/example/bee/service/MauSacService.java
@@ -1,4 +1,12 @@
 package com.example.bee.service;
 
+import com.example.bee.entity.MauSac;
+import org.springframework.data.domain.Page;
+
 public interface MauSacService {
+    public Page<MauSac> getAllMauSac(Integer totalPage, Integer size);
+    public MauSac getOneMauSac(String id);
+    public MauSac deleteMauSac(String id);
+    public MauSac updateMauSac(String id);
+
 }
--- src/main/java/com/example/bee/service/impl/MauSacServiceImpl.java
+++ src/main/java/com/example/bee/service/impl/MauSacServiceImpl.java
@@ -1,4 +1,34 @@
 package com.example.bee.service.impl;
 
-public class MauSacServiceImpl {
+import com.example.bee.entity.MauSac;
+import com.example.bee.repository.MauSacRepository;
+import com.example.bee.service.MauSacService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MauSacServiceImpl implements MauSacService {
+    @Autowired
+    private MauSacRepository REPO;
+
+    @Override
+    public Page<MauSac> getAllMauSac(Integer totalPage, Integer size) {
+        return null;
+    }
+
+    @Override
+    public MauSac getOneMauSac(String id) {
+        return null;
+    }
+
+    @Override
+    public MauSac deleteMauSac(String id) {
+        return null;
+    }
+
+    @Override
+    public MauSac updateMauSac(String id) {
+        return null;
+    }
 }
