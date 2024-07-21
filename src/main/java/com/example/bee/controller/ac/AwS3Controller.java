b71c051ea467b877299368b110ae884d99063dac
Con_Sen <90837200+vietnvph24355@users.noreply.github.com>
2024-07-21 16:11:42 +0700

    config file pom va cau hinh sws3 de luu anh

--- src/main/java/com/example/bee/controller/ac/AwS3Controller.java
+++ src/main/java/com/example/bee/controller/ac/AwS3Controller.java
@@ -1,17 +1,5 @@
 package com.example.bee.controller.ac;
 
-import com.example.bee.service.impl.AwS3Service;
-import org.apache.commons.io.FilenameUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-@CrossOrigin("*")
-@RestController
-@RequestMapping("/admin/api/file")
 public class AwS3Controller {
 
 }
--- src/main/java/com/example/bee/service/impl/AwS3Service.java
+++ src/main/java/com/example/bee/service/impl/AwS3Service.java
@@ -1,22 +1,5 @@
 package com.example.bee.service.impl;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import com.amazonaws.util.IOUtils;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-@Service
-@Slf4j
 public class AwS3Service {
 
 
