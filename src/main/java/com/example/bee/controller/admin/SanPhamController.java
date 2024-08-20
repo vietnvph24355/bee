51094607f8c59da2e0b8cbba4dc845ee0bd1f2b9
VuMaiQuan <vumaiquan28062002@gmail.com>
2024-08-20 23:52:56 +0700

    len
    Fix patten

--- src/main/java/com/example/bee/controller/admin/SanPhamController.java
+++ src/main/java/com/example/bee/controller/admin/SanPhamController.java
@@ -3,10 +3,23 @@ package com.example.bee.controller.admin;
 import com.example.bee.model.request.create_request.CreatedSanPhamRequest;
 import com.example.bee.model.request.update_request.UpdatedSanPhamRequest;
 import com.example.bee.service.SanPhamService;
+import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
 @RestController
@@ -30,8 +43,19 @@ public class SanPhamController {
     }
 
     @PostMapping()
-    public ResponseEntity<?> add(@RequestBody CreatedSanPhamRequest request) {
-        return new ResponseEntity<>(service.add(request), HttpStatus.CREATED);
+    public ResponseEntity<?> add(@RequestBody @Valid CreatedSanPhamRequest request, BindingResult result) {
+        try {
+            if (result.hasErrors()) {
+                List<String> errorMesage = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMesage);
+            }
+            return new ResponseEntity<>(service.add(request), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
@@ -40,7 +64,7 @@ public class SanPhamController {
         return ResponseEntity.noContent().build();
     }
 
-//    @PutMapping("/{id}")
+    //    @PutMapping("/{id}")
 //    public ResponseEntity<?> update(@PathVariable(name = "id") Long id, @RequestBody UpdatedSanPhamRequest request) {
 //        return ResponseEntity.ok(service.update(id, request));
 //    }
@@ -48,10 +72,12 @@ public class SanPhamController {
     public ResponseEntity<?> update(@PathVariable(name = "id") Long id, @RequestBody UpdatedSanPhamRequest request) {
         return ResponseEntity.ok(service.update(id, request));
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<?> getOne(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
+
     @GetMapping("/san-pham-moi")
     public ResponseEntity<?> get5SanPhamMoiNhat() {
         return ResponseEntity.ok(service.get5SanPhamMoiNhat());
--- src/main/java/com/example/bee/service/impl/SanPhamServiceImpl.java
+++ src/main/java/com/example/bee/service/impl/SanPhamServiceImpl.java
@@ -80,7 +80,7 @@ public class SanPhamServiceImpl implements SanPhamService {
 
     @Override
     public SanPhamResponse add(CreatedSanPhamRequest request) {
-        if (repository.existsByTen(request.getTen())) {
+        if (repository.existsByTen(request.getTen().trim())) {
             throw new BadRequestException("Tên sản phẩm đã tồn tại trong hệ thống!");
         }
         SanPham createdSanPham = mapper.convertCreateRequestToEntity(request);
@@ -121,7 +121,7 @@ public class SanPhamServiceImpl implements SanPhamService {
             throw new NotFoundException("Sản phẩm không tồn tại!");
         }
         SanPham detail = optional.get();
-        if (!request.getTen().equals(optional.get().getTen())&&repository.existsByTen(request.getTen())) {
+        if (!request.getTen().equals(optional.get().getTen().trim())&&repository.existsByTen(request.getTen().trim())) {
             throw new BadRequestException("Tên sản phẩm đã tồn tại trong hệ thống!");
         }
 
